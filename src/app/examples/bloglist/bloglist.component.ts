046f5dc2993cc7bb0fd709b911a7fe428bd41845
wizzard366 <wizzard366@outlook.com>
2024-12-19 23:47:29 -0600

    initial commit

---
+++ src/app/examples/bloglist/bloglist.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+    selector: 'app-bloglist',
+    templateUrl: './bloglist.component.html',
+    styleUrls: ['./bloglist.component.scss']
+})
+
+export class BlogListComponent implements OnInit {
+  focus: any;
+  focus1: any;
+
+  constructor() { }
+
+  ngOnInit() {}
+
+}
