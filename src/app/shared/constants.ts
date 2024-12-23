839581ac12e5513c5351db38f3400ee4fe8f3d2e
wizzard366 <wizzard366@outlook.com>
2024-12-22 18:57:27 -0600

    nav and footer updates

--- src/app/shared/constants.ts
+++ src/app/shared/constants.ts
@@ -4,5 +4,6 @@ export class AppConstants {
     public BLOG_INDEX_URL = `${this.DOMAIN}/api/v1/page/json/blog/index`;
     public BLOG_POST_BASE_URL = `${this.DOMAIN}/api/v1/page/json/blog/post`;
     public CONTENT_API_BASE_URL = `${this.DOMAIN}/api/content/id`
+    public NAVIGATION_API_URL = `${this.DOMAIN}/api/v1/nav/?depth=2`;
     
 }
--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit, ElementRef } from '@angular/core';
 import { Location, LocationStrategy, PathLocationStrategy } from '@angular/common';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AppConstants } from '../constants';
+import { AuthenticationService } from '../services/authentication.service';
 
 @Component({
     selector: 'app-navbar',
@@ -9,15 +12,37 @@ import { Location, LocationStrategy, PathLocationStrategy } from '@angular/commo
 export class NavbarComponent implements OnInit {
     private toggleButton: any;
     private sidebarVisible: boolean;
+    navigationItems:any;
 
-    constructor(public location: Location, private element : ElementRef) {
+    constructor(public location: Location, 
+        private element : ElementRef,
+        private authenticationService:AuthenticationService, 
+        private http: HttpClient, 
+        private appConstants: AppConstants) {
         this.sidebarVisible = false;
     }
 
     ngOnInit() {
         const navbar: HTMLElement = this.element.nativeElement;
         this.toggleButton = navbar.getElementsByClassName('navbar-toggler')[0];
+
+
+        this.authenticationService.getAPIToken().then(token=>{
+            this.getNavigationItems(token);
+        });
     }
+
+    getNavigationItems(token){
+        const headers = new HttpHeaders({
+            'Authorization': `Bearer ${token}`
+          })
+        this.http.get<any>(`${this.appConstants.NAVIGATION_API_URL}`,{headers}).subscribe({
+            next:(data=>{
+                this.navigationItems = data.entity.children;
+            })
+        });
+    }
+
     sidebarOpen() {
         const toggleButton = this.toggleButton;
         const html = document.getElementsByTagName('html')[0];
